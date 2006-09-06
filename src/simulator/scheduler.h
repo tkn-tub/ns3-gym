a129ff2a7da94fbe0b0b2aeb5919c0d9d0a7b1f2
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2006-09-06 14:53:11 +0200

    add scheduler example reference

--- src/simulator/scheduler.h
+++ src/simulator/scheduler.h
@@ -48,6 +48,9 @@ class EventImpl;
  * editing the main simulator class, you need to also implement
  * a subclass of the ns3::SchedulerFactory base class and
  * feed it to ns3::Simulator::setExternal.
+ * The simplest example of a scheduler is the linked-list
+ * scheduler included in ns3: see the files 
+ * src/simulator/scheduler-list.h and src/simulator/scheduler-list.cc
  */
 class Scheduler {
  public:
