50f2fc5a164fa503445f338c38961271574fc27d
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2007-06-21 11:01:15 +0200

    fix small variable name typo

--- src/common/packet.h
+++ src/common/packet.h
@@ -342,8 +342,8 @@ Packet::AddTrailer (T const &trailer)
                  "Must pass Trailer subclass to Packet::AddTrailer");
   uint32_t size = trailer.GetSize ();
   m_buffer.AddAtEnd (size);
-  Buffer::Iterator start = m_buffer.End ();
-  trailer.Serialize (start);
+  Buffer::Iterator end = m_buffer.End ();
+  trailer.Serialize (end);
   m_metadata.AddTrailer (trailer, size);
 }
 template <typename T>
