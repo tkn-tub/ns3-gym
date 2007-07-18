1ac757bd601449bbeb61d8fdc4140a8f94ed0a86
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2007-07-18 17:56:43 +0200

    get rid of old buggy random walk model

--- samples/main-random-walk.cc
+++ samples/main-random-walk.cc
@@ -1,9 +1,12 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 
+#include <vector>
+
 #include "ns3/ptr.h"
 #include "ns3/mobility-model.h"
 #include "ns3/mobility-model-notifier.h"
-#include "ns3/random-walk-mobility-model.h"
+#include "ns3/random-walk-2d-mobility-model.h"
+#include "ns3/random-topology.h"
 #include "ns3/default-value.h"
 #include "ns3/command-line.h"
 #include "ns3/simulator.h"
@@ -21,20 +24,33 @@ CourseChange (Ptr<const MobilityModel> position)
 
 int main (int argc, char *argv[])
 {
-  Bind ("RandomWalkMinSpeed", "2");
-  Bind ("RandomWalkMaxSpeed", "3");
-  Bind ("RandomWalkMode", "Time");
-  Bind ("RandomWalkModeDistance", "20");
-  Bind ("RandomWalkModeTime", "2s");
+  Bind ("RandomWalk2dMode", "Time");
+  Bind ("RandomWalk2dTime", "2s");
+  Bind ("RandomWalk2dSpeed", "Constant:1.0");
+  Bind ("RandomWalk2dBounds", "0:200:0:100");
+
+  Bind ("RandomDiscPositionX", "100");
+  Bind ("RandomDiscPositionY", "50");
+  Bind ("RandomDiscPositionRho", "Uniform:0:30");
+
+  Bind ("RandomTopologyPositionType", "RandomDiscPosition");
+  Bind ("RandomTopologyMobilityType", "RandomWalk2dMobilityModel");
 
   CommandLine::Parse (argc, argv);
 
-  Ptr<MobilityModelNotifier> notifier = Create<MobilityModelNotifier> ();
-  Ptr<RandomWalkMobilityModel> position = Create<RandomWalkMobilityModel> ();
-  position->AddInterface (notifier);
-  notifier->RegisterListener (MakeCallback (&CourseChange));
+  RandomTopology topology;
 
-  Simulator::StopAt (Seconds (20.0));
+  std::vector<Ptr<Object> > objects;
+  for (uint32_t i = 0; i < 100; i++)
+    {
+      Ptr<MobilityModelNotifier> notifier = Create<MobilityModelNotifier> ();
+      notifier->RegisterListener (MakeCallback (&CourseChange));
+      objects.push_back (notifier);
+    }
+
+  topology.Layout (objects.begin (), objects.end ());
+
+  Simulator::StopAt (Seconds (100.0));
 
   Simulator::Run ();
   
