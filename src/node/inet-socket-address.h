9bf17256fbf13f34ee1eab6b34d290225a980f95
Raj Bhattacharjea <raj.b@gatech.edu>
2007-08-24 14:00:52 -0400

    Fixed inet-socket-address's opening IFDEF to be consistent with the filename

--- src/node/inet-socket-address.h
+++ src/node/inet-socket-address.h
@@ -19,8 +19,8 @@
  * Author: Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
  */
 
-#ifndef IPV4_TRANSPORT_ADDRESS_H
-#define IPV4_TRANSPORT_ADDRESS_H
+#ifndef INET_SOCKET_ADDRESS_H
+#define INET_SOCKET_ADDRESS_H
 
 #include "address.h"
 #include "ipv4-address.h"
@@ -114,4 +114,4 @@ private:
 } // namespace ns3
 
 
-#endif /* IPV4_TRANSPORT_ADDRESS_H */
+#endif /* INET_SOCKET_ADDRESS_H */
