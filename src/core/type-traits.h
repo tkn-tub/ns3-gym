8180ca7a9d76b596857cb7620b807602561e5498
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2007-09-27 12:50:55 +0200

    add TypeTraits<T>::NonConstType

--- src/core/type-traits.h
+++ src/core/type-traits.h
@@ -8,18 +8,29 @@ template <typename T>
 struct TypeTraits
 {
   typedef T ReferencedType;
+  typedef T NonConstType;
 };
+
+template <typename T>
+struct TypeTraits<const T>
+{
+  typedef T ReferencedType;
+  typedef T NonConstType;
+};
+
 
 template <typename T>
 struct TypeTraits<const T &>
 {
   typedef T ReferencedType;
+  typedef T & NonConstType;
 };
 
 template <typename T>
 struct TypeTraits<T &>
 {
   typedef T ReferencedType;
+  typedef T & NonConstType;
 };
 
 
