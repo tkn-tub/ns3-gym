13347878df50f1421fdb73544ded91c201b0df05
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2007-09-28 10:32:59 +0200

    implement the helper IntToType template

---
+++ src/core/int-to-type.h
@@ -0,0 +1,19 @@
+#ifndef INT_TO_TYPE_H
+#define INT_TO_TYPE_H
+
+namespace ns3 {
+
+/**
+ * This trivial template is extremely useful, as explained in
+ * "Modern C++ Design", p29, section 2.4, 
+ * "Mapping Integral Constants to Types"
+ */
+template <int v>
+struct IntToType
+{
+  enum {value = v};
+};
+
+} // namespace ns3
+
+#endif /* INT_TO_TYPE_H */
