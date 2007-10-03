5a7965468d8f9986ce7a2f15c0f961f3872d1e32
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2007-10-03 13:11:28 +0200

    add MacStation to build

---
+++ src/devices/wifi/mac-station.cc
@@ -0,0 +1,66 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2005,2006 INRIA
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as 
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
+ */
+
+#include "mac-station.h"
+#include "mac-stations.h"
+
+namespace ns3 {
+
+MacStation::MacStation ()
+  : m_state (DISASSOC)
+{}
+
+
+MacStation::~MacStation ()
+{}
+
+bool 
+MacStation::IsAssociated (void) const
+{
+  return m_state == GOT_ASSOC_TX_OK;
+}
+bool 
+MacStation::IsWaitAssocTxOk (void) const
+{
+  return m_state == WAIT_ASSOC_TX_OK;
+}
+void 
+MacStation::RecordWaitAssocTxOk (void)
+{
+  m_state = WAIT_ASSOC_TX_OK;
+}
+void 
+MacStation::RecordGotAssocTxOk (void)
+{
+  m_state = GOT_ASSOC_TX_OK;
+}
+void 
+MacStation::RecordGotAssocTxFailed (void)
+{
+  m_state = DISASSOC;
+}
+void 
+MacStation::RecordDisassociated (void)
+{
+  m_state = DISASSOC;
+}
+
+}; // namespace ns3
