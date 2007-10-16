2922488eb974b425d1759d39371a2aa3919b15c8
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2007-10-16 13:32:19 +0200

    add wrapper class around RngStream for testing.

---
+++ src/devices/wifi/random-stream.cc
@@ -0,0 +1,54 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2007 INRIA
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as 
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
+ */
+#include "random-stream.h"
+#include "ns3/rng-stream.h"
+#include "ns3/assert.h"
+
+namespace ns3 {
+
+RandomStream::~RandomStream ()
+{}
+
+
+RealRandomStream::RealRandomStream ()
+{}
+uint32_t 
+RealRandomStream::GetNext (uint32_t min, uint32_t max)
+{
+  return m_stream.RandInt (min, max);
+}
+
+
+void 
+TestRandomStream::AddNext (uint32_t v)
+{
+  m_nexts.push_back (v);
+}
+
+uint32_t 
+TestRandomStream::GetNext (uint32_t min, uint32_t max)
+{
+  NS_ASSERT (!m_nexts.empty ());
+  uint32_t next = m_nexts.front ();
+  m_nexts.pop_front ();
+  return next;
+}
+
+} // namespace ns3
---
+++ src/devices/wifi/random-stream.h
@@ -0,0 +1,60 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2007 INRIA
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as 
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
+ */
+#ifndef RANDOM_STREAM_H
+#define RANDOM_STREAM_H
+
+#include <stdint.h>
+#include <list>
+#include "ns3/rng-stream.h"
+
+namespace ns3 {
+
+/**
+ * A simple wrapper around RngStream to make testing 
+ * of the code easier.
+ */
+class RandomStream
+{
+public:
+  virtual ~RandomStream ();
+  virtual uint32_t GetNext (uint32_t min, uint32_t max) = 0;
+};
+
+class RealRandomStream : public RandomStream
+{
+public:
+  RealRandomStream ();
+  virtual uint32_t GetNext (uint32_t min, uint32_t max);
+private:
+  RngStream m_stream;
+};
+
+class TestRandomStream : public RandomStream
+{
+public:
+  void AddNext (uint32_t v);
+  virtual uint32_t GetNext (uint32_t min, uint32_t max);
+private:
+  std::list<uint32_t> m_nexts;
+};
+
+} // namespace ns3
+
+#endif /* RANDOM_STREAM_H */
