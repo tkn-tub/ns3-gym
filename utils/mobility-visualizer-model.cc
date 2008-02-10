450f9db5c6b763334c888eff955de9ca2ac40e9b
Gustavo J. A. M. Carneiro <gjc@inescporto.pt>
2008-02-10 11:51:53 +0000

    RandomWalk2dMobilityModel changed name to RandomWalkMobilityModel (typo or intentional?)

--- utils/mobility-visualizer-model.cc
+++ utils/mobility-visualizer-model.cc
@@ -82,7 +82,7 @@ int model_init (int argc, char *argv[], double *x1, double *y1, double *x2, doub
 //   DefaultValue::Bind ("RandomDiscPositionRho", "Uniform:0:30");
 
   DefaultValue::Bind ("RandomTopologyPositionType", "RandomRectanglePosition");
-  DefaultValue::Bind ("RandomTopologyMobilityType", "RandomWalk2dMobilityModel");
+  DefaultValue::Bind ("RandomTopologyMobilityType", "RandomWalkMobilityModel");
 
 //   CommandLine::AddArgValue ("sample-interval","sample interval", g_sampleInterval);
 //   CommandLine::AddArgValue ("num-nodes","number of nodes", g_numNodes);
@@ -102,7 +102,7 @@ int model_init (int argc, char *argv[], double *x1, double *y1, double *x2, doub
   Simulator::Schedule (g_sampleInterval, Sample);
 
   TypeId mobType = DefaultValueListGet<TypeIdDefaultValue> ("RandomTopologyMobilityType")->GetValue ();
-  if (mobType.GetName () == "RandomWalk2dMobilityModel")
+  if (mobType.GetName () == "RandomWalkMobilityModel")
     {
       Rectangle bounds = DefaultValueListGet<RectangleDefaultValue> ("RandomWalk2dBounds")->GetValue ();
       *x1 = bounds.xMin;
