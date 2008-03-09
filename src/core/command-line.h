517c5e5bfdb6f36246dd43326544133b68908e25
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2008-03-10 00:38:48 +0100

    add missing copyright statements.

--- src/core/command-line.h
+++ src/core/command-line.h
@@ -1,5 +1,24 @@
-#ifndef CMD_LINE_H
-#define CMD_LINE_H
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2008 INRIA
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
+ */
+#ifndef COMMAND_LINE_H
+#define COMMAND_LINE_H
 
 #include <string>
 #include <sstream>
@@ -75,4 +94,4 @@ CommandLine::UserItem<T>::Parse (std::string value)
 
 } // namespace ns3
 
-#endif /* CMD_LINE_H */
+#endif /* COMMAND_LINE_H */
--- src/core/double.h
+++ src/core/double.h
@@ -1,5 +1,24 @@
-#ifndef FP_VALUE_H
-#define FP_VALUE_H
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2008 INRIA
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
+ */
+#ifndef NS_DOUBLE_H
+#define NS_DOUBLE_H
 
 #include "attribute.h"
 #include "attribute-helper.h"
@@ -58,5 +77,4 @@ Ptr<const AttributeChecker> MakeDoubleChecker (double min)
 
 } // namespace ns3
 
-
-#endif /* FP_VALUE_H */
+#endif /* NS_DOUBLE_H */
