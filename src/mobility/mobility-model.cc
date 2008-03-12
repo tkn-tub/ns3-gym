5a7be54e3168bde5efa954921b74ee601e69a3c8
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2008-03-12 10:18:14 -0700

    make sure attributes always start with a capital letter.

--- src/mobility/mobility-model.cc
+++ src/mobility/mobility-model.cc
@@ -28,13 +28,13 @@ MobilityModel::GetTypeId (void)
 {
   static TypeId tid = TypeId ("MobilityModel")
     .SetParent<Object> ()
-    .AddAttribute ("position", "The current position of the mobility model.",
+    .AddAttribute ("Position", "The current position of the mobility model.",
                    TypeId::ATTR_SGC,
                    Vector (0.0, 0.0, 0.0),
                    MakeVectorAccessor (&MobilityModel::SetPosition,
                                         &MobilityModel::GetPosition),
                    MakeVectorChecker ())
-    .AddAttribute ("velocity", "The current velocity of the mobility model.",
+    .AddAttribute ("Velocity", "The current velocity of the mobility model.",
                    TypeId::ATTR_GET,
                    Vector (0.0, 0.0, 0.0), // ignored initial value.
                    MakeVectorAccessor (&MobilityModel::GetVelocity),
