29f19e164e6b807d1b0c58f56cfdedd92afd858a
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2008-03-24 10:14:35 -0700

    doxygen.

--- src/helper/net-device-container.h
+++ src/helper/net-device-container.h
@@ -7,23 +7,49 @@
 
 namespace ns3 {
 
+/**
+ * \brief holds a vector of ns3::NetDevice pointers
+ *
+ */
 class NetDeviceContainer
 {
 public:
   typedef std::vector<Ptr<NetDevice> >::const_iterator Iterator;
 
+  /**
+   * \returns an iterator which points to the start of the array of pointers.
+   */
   Iterator Begin (void) const;
+  /**
+   * \returns an iterator which points to the end of the array of pointers.
+   */
   Iterator End (void) const;
 
+  /**
+   * \returns the number of netdevice pointers stored in this container.
+   */
   uint32_t GetN (void) const;
+  /**
+   * \param i the index of the requested netdevice pointer.
+   * \returns the requested netdevice pointer.
+   */
   Ptr<NetDevice> Get (uint32_t i) const;
 
-  void Create (uint32_t n);
+  /**
+   * \param other another netdevice container
+   *
+   * Append to the end of this container the other input container.
+   */
   void Add (NetDeviceContainer other);
-  void Add (Ptr<NetDevice> node);
+  /**
+   * \param device another netdevice pointer.
+   *
+   * Append to the end of this container the input netdevice pointer.
+   */
+  void Add (Ptr<NetDevice> device);
 
 private:
-  std::vector<Ptr<NetDevice> > m_nodes;
+  std::vector<Ptr<NetDevice> > m_devices;
 };
 
 } // namespace ns3
