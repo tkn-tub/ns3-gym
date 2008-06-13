cbc938e2bfcf7029c435a83c77876047cd2c2579
Tom Henderson <tomh@tomh.org>
2008-06-12 22:21:19 -0700

    OLSR doxygen

--- src/routing/olsr/olsr.h
+++ src/routing/olsr/olsr.h
@@ -23,6 +23,39 @@
 
 #include "ns3/node-list.h"
 
+/**
+ * \ingroup routing
+ * \defgroup olsr OLSR
+ *
+ * \section model Model
+ *
+ * This model implements the base specification of the Optimized
+ * Link State Routing (OLSR) protocol, which is a dynamic mobile ad hoc
+ * unicast routing protocol.  It has been developed at the 
+ * University of Murcia (Spain) by Francisco J. Ros for ns-2, and was
+ * ported to ns-3 by Gustavo Carneiro.
+ *
+ * Here is a summary of software's main features:
+ *  - Compliant with core-OLSR (but the use of multiple interfaces which is currently not supported by NS2) as it is documented in RFC 3626 
+ * (http://www.ietf.org/rfc/rfc3626.txt).
+ *  -  Support for MAC layer feedback as described in RFC 3626.
+ *
+ * \section api API and Usage
+ * 
+ * A helper class for OLSR has been written.  After an IPv4 topology
+ * has been created and assigned IP addresses, the simulation script 
+ * writer can call one of three overloaded functions with different scope 
+ * to enable OLSR:  ns3::OlsrHelper::Install (NodeContainer container);
+ * ns3::OlsrHelper::Install (Ptr<Node> node);
+ * or ns3::OlsrHelper::InstallAll (void);
+ *
+ * In addition, the behavior of OLSR can be modified by changing certain
+ * attributes.  The method ns3::OlsrHelper::SetAgent () can be used
+ * to set OLSR attributes.  These include HelloInterval, TcInterval,
+ * MidInterval, Willingness.  Other parameters are defined as macros
+ * in olsr-agent-impl.cc.
+ */
+
 namespace ns3
 {
   /**
