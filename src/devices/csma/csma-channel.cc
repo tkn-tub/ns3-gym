f3d8a5eb6dfef72e6b1b158206ce1c641be83fdf
Gustavo J. A. M. Carneiro <gjc@inescporto.pt>
2008-07-04 17:58:31 +0100

    Don't allow csma devices to receive packets sent by themselves.

--- src/devices/csma/csma-channel.cc
+++ src/devices/csma/csma-channel.cc
@@ -232,12 +232,14 @@ CsmaChannel::PropagationCompleteEvent()
   NS_LOG_LOGIC ("Receive");
   
   std::vector<CsmaDeviceRec>::iterator it;
+  uint32_t devId = 0;
   for (it = m_deviceList.begin (); it < m_deviceList.end(); it++) 
     {
-      if (it->IsActive ())
+      if (it->IsActive () && m_currentSrc != devId)
       {
         it->devicePtr->Receive (m_currentPkt->Copy ());
       }
+      devId++;
     }
   m_state = IDLE;
 }
