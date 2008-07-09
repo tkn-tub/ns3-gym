c3a8ec555549e1299754a2cb298c50451542c2a9
Raj Bhattacharjea <raj.b@gatech.edu>
2008-07-09 17:04:29 -0400

    Add David Evensky's tcp-2way regression test

---
+++ regression/tests/test-tcp-2way.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+"""Generic trace-comparison-type regression test."""
+
+import os
+import shutil
+import tracediff
+
+def run(verbose, generate, refDirName):
+    """Execute a test."""
+
+    return tracediff.run_test(verbose, generate, refDirName,
+        "tcp-2way")
