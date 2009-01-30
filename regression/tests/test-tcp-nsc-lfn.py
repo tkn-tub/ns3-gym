33c6cd365dcecde37a48e75874f297236a3e2b1c
Gustavo J. A. M. Carneiro <gjc@inescporto.pt>
2009-01-30 19:28:00 +0000

    Skip the NSC-based regression test on non-linux platforms

--- regression/tests/test-tcp-nsc-lfn.py
+++ regression/tests/test-tcp-nsc-lfn.py
@@ -3,7 +3,7 @@
 """Trace-comparison-type regression test for the Network Simulation Cradle."""
 
 import platform
-
+import sys
 
 def may_run(env, options):
     if not env['NSC_ENABLED']:
@@ -11,6 +11,8 @@ def may_run(env, options):
     else:
         if options.valgrind:
             return "NSC does not get along with valgrind"
+        if sys.platform != 'linux2':
+            return "NSC is not well supported on non-Linux platforms"
         return 0
 
 
