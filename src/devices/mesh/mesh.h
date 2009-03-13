9fd6929b5508c01b138ef6d6ba4e619ac49ca378
Pavel Boyko <boyko@iitp.ru>
2009-03-13 14:08:43 +0300

    src/devices/mesh directory tree and doxygen module created.

---
+++ src/devices/mesh/mesh.h
@@ -0,0 +1,30 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/* 
+ * Copyright (c) 2009 IITP RAS
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * Author: Pavel Boyko <boyko@iitp.ru>
+ * 
+ * This is toplevel mesh module description
+ */
+
+/**
+ * \ingroup devices
+ * \defgroup mesh Mesh
+ * 
+ * \brief Level 2 mobile ad hoc wireless networking
+ * 
+ * TODO module documentation here
+ */
--- src/devices/wifi/mesh-wifi-perr-information-element.h
+++ src/devices/wifi/mesh-wifi-perr-information-element.h
@@ -32,6 +32,7 @@
 
 namespace ns3
 {
+
 class WifiPerrInformationElement : public Header
 {
 	public:
