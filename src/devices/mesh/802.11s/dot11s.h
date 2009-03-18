39072b6d5dbef9aa35b537b39b9ac23bba01cc66
Pavel Boyko <boyko@iitp.ru>
2009-03-18 17:56:53 +0300

    mesh/dot11s doxygen module added

---
+++ src/devices/mesh/802.11s/dot11s.h
@@ -0,0 +1,29 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/* 
+ * Copyright (c) 2009 IITP RAS
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * Author: Pavel Boyko <boyko@iitp.ru>
+ * 
+ * This is doxygen module description, don't include 
+ */
+/**
+ * \ingroup mesh 
+ * \defgroup dot11s IEEE 802.11s draft
+ * 
+ * \brief IEEE 802.11s (mesh) draft standard implementation
+ * 
+ * TODO: add documentation on 802.11s implementation here
+ */
