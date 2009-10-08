a233d593022cd1d7cbf3fcea55e382b6f737e995
Sebastien Vincent <vincent@clarinet.u-strasbg.fr>
2009-10-08 17:31:58 +0200

    Fix valgrind error for icmpv6-redirect example.

--- src/internet-stack/icmpv6-l4-protocol.cc
+++ src/internet-stack/icmpv6-l4-protocol.cc
@@ -962,7 +962,8 @@ void Icmpv6L4Protocol::SendRedirection (Ptr<Packet> redirectedPacket, Ipv6Addres
 
   if ((redirectedPacketSize % 8) != 0)
   {
-    redirectedPacket->AddPaddingAtEnd (8 - (redirectedPacketSize % 8));
+    Ptr<Packet> pad = Create<Packet>(8 - (redirectedPacketSize % 8));
+    redirectedPacket->AddAtEnd (pad); 
   }
 
   if (redirHardwareTarget.GetLength ())
