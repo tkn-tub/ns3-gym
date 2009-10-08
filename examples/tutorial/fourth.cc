914b005571fe32491c138049127260e9de4ce633
Craig Dowell <craigdo@ee.washington.edu>
2009-10-08 00:16:55 -0700

    Words about tracing in tutorial

---
+++ examples/tutorial/fourth.cc
@@ -0,0 +1,58 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "ns3/object.h"
+#include "ns3/uinteger.h"
+#include "ns3/traced-value.h"
+#include "ns3/trace-source-accessor.h"
+
+#include <iostream>
+  
+using namespace ns3;
+
+class MyObject : public Object
+{
+public:
+  static TypeId GetTypeId (void)
+  {
+    static TypeId tid = TypeId ("MyObject")
+      .SetParent (Object::GetTypeId ())
+      .AddConstructor<MyObject> ()
+      .AddTraceSource ("MyInteger",
+                       "An integer value to trace.",
+                       MakeTraceSourceAccessor (&MyObject::m_myInt))
+      ;
+    return tid;
+  }
+  
+  MyObject () {}
+  TracedValue<int32_t> m_myInt;
+};
+
+void
+IntTrace (int32_t oldValue, int32_t newValue)
+{
+  std::cout << "Traced " << oldValue << " to " << newValue << std::endl;
+}
+
+int
+main (int argc, char *argv[])
+{
+  Ptr<MyObject> myObject = CreateObject<MyObject> ();
+  myObject->TraceConnectWithoutContext ("MyInteger", MakeCallback(&IntTrace));
+
+  myObject->m_myInt = 1234;
+}
