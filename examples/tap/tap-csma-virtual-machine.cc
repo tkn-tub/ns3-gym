ae08ebf57928429dd02b1098ce0160d8c1531596
Craig Dowell <craigdo@ee.washington.edu>
2010-02-18 10:58:39 -0800

    comments are helpful

--- examples/tap/tap-csma-virtual-machine.cc
+++ examples/tap/tap-csma-virtual-machine.cc
@@ -80,25 +80,54 @@ main (int argc, char *argv[])
   CommandLine cmd;
   cmd.Parse (argc, argv);
 
+  //
+  // We are interacting with the outside, real, world.  This means we have to 
+  // interact in real-time and therefore means we have to use the real-time
+  // simulator and take the time to calculate checksums.
+  //
   GlobalValue::Bind ("SimulatorImplementationType", StringValue ("ns3::RealtimeSimulatorImpl"));
   GlobalValue::Bind ("ChecksumEnabled", BooleanValue (true));
 
+  //
+  // Create two ghost nodes.  The first will represent the virtual machine host
+  // on the left side of the network; and the second will represent the VM on 
+  // the right side.
+  //
   NodeContainer nodes;
   nodes.Create (2);
 
+  //
+  // Use a CsmaHelper to get a CSMA channel created, and the needed net 
+  // devices installed on both of the nodes.  The data rate and delay for the
+  // channel can be set through the command-line parser.  For example,
+  //
+  // ./waf --run "tap=csma-virtual-machine --ns3::CsmaChannel::DataRate=10000000"
+  //
   CsmaHelper csma;
   NetDeviceContainer devices = csma.Install (nodes);
 
+  //
+  // Use the TapBridgeHelper to connect to the pre-configured tap devices for 
+  // the left side.  We go with "UseBridge" mode since the CSMA devices support
+  // promiscuous mode and can therefore make it appear that the bridge is 
+  // extended into ns-3.  The install method essentially bridges the specified
+  // tap to the specified CSMA device.
+  //
   TapBridgeHelper tapBridge;
   tapBridge.SetAttribute ("Mode", StringValue ("UseBridge"));
   tapBridge.SetAttribute ("DeviceName", StringValue ("tap-left"));
   tapBridge.Install (nodes.Get (0), devices.Get (0));
 
+  //
+  // Connect the right side tap to the right side CSMA device on the right-side
+  // ghost node.
+  //
   tapBridge.SetAttribute ("DeviceName", StringValue ("tap-right"));
   tapBridge.Install (nodes.Get (1), devices.Get (1));
 
-  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-
+  //
+  // Run the simulation for ten minutes to give the user time to play around
+  //
   Simulator::Stop (Seconds (600.));
   Simulator::Run ();
   Simulator::Destroy ();
