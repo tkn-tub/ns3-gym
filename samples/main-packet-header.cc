99571b924a156957a59c80a10271962e7c4dab4e
David <david.rua@gmail.com>
2010-07-20 21:10:46 -0700

    Fix main-packet-header.cc sample (bug 918)

--- samples/main-packet-header.cc
+++ samples/main-packet-header.cc
@@ -41,6 +41,7 @@ MyHeader::GetTypeId (void)
 {
   static TypeId tid = TypeId ("ns3::MyHeader")
     .SetParent<Header> ()
+    .AddConstructor<MyHeader> ()
     ;
   return tid;
 }
@@ -55,7 +56,8 @@ MyHeader::Print (std::ostream &os) const
 {
   // This method is invoked by the packet printing
   // routines to print the content of my header.
-  os << "data=" << m_data << std::endl;
+  //os << "data=" << m_data << std::endl;
+  os << "data=" << m_data;
 }
 uint32_t
 MyHeader::GetSerializedSize (void) const
@@ -109,6 +111,7 @@ int main (int argc, char *argv[])
 
   // print the content of my packet on the standard output.
   p->Print (std::cout);
+  std::cout << std::endl;  
 
   // you can now remove the header from the packet:
   MyHeader destinationHeader;
