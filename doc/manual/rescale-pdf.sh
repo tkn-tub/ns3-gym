2dde593a318d5cadb16636515f354e93edb97362
Tom Henderson <tomh@tomh.org>
2011-01-02 22:56:16 -0800

    fix escape character

--- doc/manual/rescale-pdf.sh
+++ doc/manual/rescale-pdf.sh
@@ -4,7 +4,7 @@ TMPFILE=`mktemp`
 
 echo "\documentclass{book}
 \usepackage{pdfpages}
-\\begin{document}
+\\\begin{document}
 \includepdf[width=${1},fitpaper]{${2}}
 \end{document}" >${TMPFILE}.tex
 pdflatex -output-directory /tmp ${TMPFILE}.tex >/dev/null 2>/dev/null
