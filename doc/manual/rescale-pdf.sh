ffed225c73e1c90e77b12404f8c450daaaf7e951
Tom Henderson <tomh@tomh.org>
2011-02-24 10:04:21 -0800

    bug 1062: fix portability issue with rescale-pdf.sh

--- doc/manual/rescale-pdf.sh
+++ doc/manual/rescale-pdf.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 TMPFILE=`mktemp`
 
 echo "\documentclass{book}
 \usepackage{pdfpages}
-\\begin{document}
+\begin{document}
 \includepdf[width=${1},fitpaper]{${2}}
 \end{document}" >${TMPFILE}.tex
 pdflatex -output-directory /tmp ${TMPFILE}.tex >/dev/null 2>/dev/null
