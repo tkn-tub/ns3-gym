6c091a90303754d8610d8be84e331472dab7a6ba
Tom Henderson <tomh@tomh.org>
2011-02-25 15:27:30 -0800

    Fix sample dependency

--- samples/main-grid-topology.cc
+++ samples/main-grid-topology.cc
@@ -1,7 +1,7 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 
 #include "ns3/core-module.h"
-#include "ns3/helper-module.h"
+#include "ns3/network-module.h"
 #include "ns3/mobility-module.h"
 
 
