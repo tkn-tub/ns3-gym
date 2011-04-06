14dda92abb6e639848da654401a6564135b448b2
Nicola Baldo <nbaldo@cttc.es>
2011-04-06 10:15:28 +0200

    input file for default attribute values

--- src/lte/examples/lena-first-sim.cc
+++ src/lte/examples/lena-first-sim.cc
@@ -23,11 +23,29 @@
 #include "ns3/network-module.h"
 #include "ns3/mobility-module.h"
 #include "ns3/lte-module.h"
-//#include "ns3/gtk-config-store.h"
+#include "ns3/config-store.h"
+
 using namespace ns3;
 
 int main (int argc, char *argv[])
 {
+
+
+  
+
+  CommandLine cmd;
+  cmd.Parse (argc, argv);
+
+  // to save a template default attribute file run it like this:
+  // ./waf --command-template="%s --ns3::ConfigStore::Filename=input-defaults.txt --ns3::ConfigStore::Mode=Save --ns3::ConfigStore::FileFormat=RawText" --run src/lte/examples/lena-first-sim
+  //
+  // to load a previously created default attribute file
+  // ./waf --command-template="%s --ns3::ConfigStore::Filename=input-defaults.txt --ns3::ConfigStore::Mode=Load --ns3::ConfigStore::FileFormat=RawText" --run src/lte/examples/lena-first-sim
+  // note that the latter will override any of the defaults set via command-line arguments
+  //
+  ConfigStore inputConfig;
+  inputConfig.ConfigureDefaults ();
+
   LenaHelper lena;
 
   lena.EnableLogComponents ();
@@ -65,9 +83,6 @@ int main (int argc, char *argv[])
   Simulator::Stop (Seconds (0.005));
 
   Simulator::Run ();
-
-  //GtkConfigStore config;
-  //config.ConfigureAttributes ();
 
   Simulator::Destroy ();
   return 0;
