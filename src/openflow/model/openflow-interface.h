d3f514eb633492256ac501c22c3e2588840ec4c4
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2011-04-06 15:02:54 -0400

    openflow private headers

--- src/openflow/model/openflow-interface.h
+++ src/openflow/model/openflow-interface.h
@@ -37,9 +37,9 @@
 #include <limits>
 
 // Include main header and Vendor Extension files
-#include "openflow.h"
-#include "nicira-ext.h"
-#include "ericsson-ext.h"
+#include "openflow/openflow.h"
+#include "openflow/nicira-ext.h"
+#include "openflow/ericsson-ext.h"
 
 extern "C"
 {
@@ -49,21 +49,21 @@ extern "C"
 #define list List
 
 // Include OFSI Library files
-#include "csum.h"
-#include "poll-loop.h"
-#include "rconn.h"
-#include "stp.h"
-#include "vconn.h"
-#include "xtoxll.h"
+#include "openflow/private/csum.h"
+#include "openflow/private/poll-loop.h"
+#include "openflow/private/rconn.h"
+#include "openflow/private/stp.h"
+#include "openflow/private/vconn.h"
+#include "openflow/private/xtoxll.h"
 
 // Include OFSI Switch files
-#include "chain.h"
-#include "table.h"
-#include "datapath.h" // The functions below are defined in datapath.c
+#include "openflow/private/chain.h"
+#include "openflow/private/table.h"
+#include "openflow/private/datapath.h" // The functions below are defined in datapath.c
 uint32_t save_buffer (ofpbuf *);
 ofpbuf * retrieve_buffer (uint32_t id);
 void discard_buffer (uint32_t id);
-#include "dp_act.h" // The functions below are defined in dp_act.c
+#include "openflow/private/dp_act.h" // The functions below are defined in dp_act.c
 void set_vlan_vid (ofpbuf *buffer, sw_flow_key *key, const ofp_action_header *ah);
 void set_vlan_pcp (ofpbuf *buffer, sw_flow_key *key, const ofp_action_header *ah);
 void strip_vlan (ofpbuf *buffer, sw_flow_key *key, const ofp_action_header *ah);
@@ -72,7 +72,7 @@ void set_nw_addr (ofpbuf *buffer, sw_flow_key *key, const ofp_action_header *ah)
 void set_tp_port (ofpbuf *buffer, sw_flow_key *key, const ofp_action_header *ah);
 void set_mpls_label (ofpbuf *buffer, sw_flow_key *key, const ofp_action_header *ah);
 void set_mpls_exp (ofpbuf *buffer, sw_flow_key *key, const ofp_action_header *ah);
-#include "pt_act.h" // The function below is defined in pt_act.c
+#include "openflow/private/pt_act.h" // The function below is defined in pt_act.c
 void update_checksums (ofpbuf *buffer, const sw_flow_key *key, uint32_t old_word, uint32_t new_word);
 
 #undef list
