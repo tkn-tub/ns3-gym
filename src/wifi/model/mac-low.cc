8f5bb9a83384fdd9d4537147d2c61eeddfa856ee
Ruben Merz <ruben@net.t-labs.tu-berlin.de>
2011-05-16 22:12:39 +0200

    [Bug 827] promiscuous mode for wifi

--- src/wifi/model/mac-low.cc
+++ src/wifi/model/mac-low.cc
@@ -366,6 +366,7 @@ MacLow::MacLow ()
   NS_LOG_FUNCTION (this);
   m_lastNavDuration = Seconds (0);
   m_lastNavStart = Seconds (0);
+  m_promisc = false;
 }
 
 MacLow::~MacLow ()
@@ -521,6 +522,11 @@ void
 MacLow::SetBssid (Mac48Address bssid)
 {
   m_bssid = bssid;
+}
+void
+MacLow::SetPromisc (void)
+{
+  m_promisc = true;
 }
 Mac48Address
 MacLow::GetAddress (void) const
@@ -891,6 +897,14 @@ MacLow::ReceiveOk (Ptr<Packet> packet, double rxSnr, WifiMode txMode, WifiPreamb
       else
         {
           // DROP
+        }
+    }
+  else if (m_promisc)
+    {
+      NS_ASSERT (hdr.GetAddr1 () != m_self);
+      if (hdr.IsData ())
+        {
+          goto rxPacket;
         }
     }
   else
