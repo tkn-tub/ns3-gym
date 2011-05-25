62a54ebaed147ff50b7a3ecde42c2b553378c948
Tom Henderson <tomh@tomh.org>
2011-05-25 00:40:33 -0700

    Fix tap-bridge doxygen group

--- src/tap-bridge/doc/tap.h
+++ src/tap-bridge/doc/tap.h
@@ -1,5 +1,5 @@
 /**
- * \defgroup TapBridgeModel Tap Bridge Device
+ * \defgroup tap-bridge Tap Bridge Device
  *
  * This section documents the API of the ns-3 tap-bridge module. For a
  * generic functional description, please refer to the ns-3 manual.
