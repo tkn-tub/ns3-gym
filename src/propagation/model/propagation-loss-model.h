4943cf9f386ea9069eaaf31f2541d3f0843b4cf5
Gustavo J. A. M. Carneiro <gjc@inescporto.pt>
2011-11-26 08:43:33 -0800

    Missing PropagationLossModel.CalcRxPower in Python bindings

--- src/propagation/model/propagation-loss-model.h
+++ src/propagation/model/propagation-loss-model.h
@@ -27,6 +27,7 @@
 
 #include "ns3/object.h"
 #include "ns3/random-variable.h"
+#include "ns3/mobility-model.h"
 #include <map>
 
 namespace ns3 {
@@ -35,8 +36,6 @@ namespace ns3 {
  * \defgroup propagation Propagation Models
  *
  */
-
-class MobilityModel;
 
 /**
  * \ingroup propagation
