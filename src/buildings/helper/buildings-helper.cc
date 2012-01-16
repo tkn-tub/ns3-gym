37b11bacbf0fc355a2e5ab81bb0734a7180704ad
Nicola Baldo <nbaldo@cttc.es>
2012-01-16 13:09:00 +0100

    fixed copyright year

--- src/buildings/helper/buildings-helper.cc
+++ src/buildings/helper/buildings-helper.cc
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2010 CTTC
+ * Copyright (c) 2012 CTTC
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
