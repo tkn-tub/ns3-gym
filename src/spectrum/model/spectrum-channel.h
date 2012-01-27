f6dea45d3f49c02c2fd1abf83edc55332397693d
Nicola Baldo <nbaldo@cttc.es>
2012-01-27 13:34:21 +0100

    Backed out changeset 4850866f297b (SpectrumChannel::RemoveRx)

--- src/spectrum/model/spectrum-channel.h
+++ src/spectrum/model/spectrum-channel.h
@@ -79,8 +79,7 @@ public:
   virtual void StartTx (Ptr<SpectrumSignalParameters> params) = 0;
 
   /**
-   * @brief add a SpectrumPhy to a channel, so it can receive signals
-   * transmitted over the channel 
+   * @brief add a SpectrumPhy to a channel, so it can receive packets
    *
    * This method is used to attach a SpectrumPhy instance to a
    * SpectrumChannel instance, so that the SpectrumPhy can receive
@@ -95,15 +94,6 @@ public:
    * a receiver.
    */
   virtual void AddRx (Ptr<SpectrumPhy> phy) = 0;
-
-  /**
-   * @brief remove a previously added SpectrumPhy from the channel, so
-   * that it will not receive any more signals transmitted on the channel
-   *
-   *
-   * @param phy the SpectrumPhy instance to be removed from the channel
-   */
-  virtual void RemoveRx (Ptr<SpectrumPhy> phy) = 0;
 
 };
 
