e1da179b80e4437372a23398f9b1234abb4da643
Gustavo J. A. M. Carneiro <gjc@inescporto.pt>
2012-02-20 16:45:50 +0000

    Bug 1349 - TypeId.LookupAttributeByName Python bindings missing

--- src/core/bindings/modulegen__gcc_LP64.py
+++ src/core/bindings/modulegen__gcc_LP64.py
@@ -1429,7 +1429,7 @@ def register_Ns3TypeId_methods(root_module, cls):
     ## type-id.h (module 'core'): bool ns3::TypeId::LookupAttributeByName(std::string name, ns3::TypeId::AttributeInformation * info) const [member function]
     cls.add_method('LookupAttributeByName', 
                    'bool', 
-                   [param('std::string', 'name'), param('ns3::TypeId::AttributeInformation *', 'info')], 
+                   [param('std::string', 'name'), param('ns3::TypeId::AttributeInformation *', 'info', transfer_ownership=False)], 
                    is_const=True)
     ## type-id.h (module 'core'): static ns3::TypeId ns3::TypeId::LookupByName(std::string name) [member function]
     cls.add_method('LookupByName', 
