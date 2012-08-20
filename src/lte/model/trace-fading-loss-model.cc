6eaad80e8fb4b26f8156b818b5372460003dafa6
Marco Miozzo <marco.miozzo@cttc.es>
2012-08-20 13:44:34 +0200

    Bug-fix in TraceFadingLossModel: windows offset update

--- src/lte/model/trace-fading-loss-model.cc
+++ src/lte/model/trace-fading-loss-model.cc
@@ -162,12 +162,15 @@ TraceFadingLossModel::DoCalcRxPowerSpectralDensity (
     {
       if (Simulator::Now ().GetSeconds () >= m_lastWindowUpdate.GetSeconds () + m_windowSize.GetSeconds ())
         {
-          NS_LOG_INFO ("Fading Window Updated");
-          std::map <ChannelRealizationId_t, UniformVariable* >::iterator itVar;
-
-        itVar = m_startVariableMap.find (mobilityPair);
-        (*itOff).second = (*itVar).second->GetValue ();
-
+          // update all the offsets
+          NS_LOG_INFO ("Fading Windows Updated");
+          std::map <ChannelRealizationId_t, int >::iterator itOff2;
+          for (itOff2 = m_windowOffsetsMap.begin (); itOff2 != m_windowOffsetsMap.end (); itOff2++)
+            {
+              std::map <ChannelRealizationId_t, UniformVariable* >::iterator itVar;
+              itVar = m_startVariableMap.find ((*itOff2).first);
+              (*itOff2).second = (*itVar).second->GetValue ();
+            }
           m_lastWindowUpdate = Simulator::Now ();
         }
     }
@@ -201,7 +204,7 @@ TraceFadingLossModel::DoCalcRxPowerSpectralDensity (
       if (*vit != 0.)
         {
           double fading = m_fadingTrace.at (subChannel).at (index);
-          //NS_LOG_INFO (this << " offset " << (*itOff).second << " fading " << fading);
+          NS_LOG_INFO (this << " FADING now " << now_ms << " offset " << (*itOff).second << " id " << index << " fading " << fading);
           double power = *vit; // in Watt/Hz
           power = 10 * log10 (180000 * power); // in dB
 
