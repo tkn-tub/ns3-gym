b4291203c5b0400e849946db944fa97836a09d78
Tom Henderson <tomh@tomh.org>
2012-08-26 23:23:52 -0700

    bug 1256: Unnecessary SND.NXT advance, missing ACK for Out of Order segments

--- src/internet/model/tcp-socket-base.cc
+++ src/internet/model/tcp-socket-base.cc
@@ -1236,7 +1236,7 @@ TcpSocketBase::ProcessWait (Ptr<Packet> packet, const TcpHeader& tcpHeader)
   // Extract the flags. PSH and URG are not honoured.
   uint8_t tcpflags = tcpHeader.GetFlags () & ~(TcpHeader::PSH | TcpHeader::URG);
 
-  if (packet->GetSize () > 0)
+  if (packet->GetSize () > 0 && tcpflags != TcpHeader::ACK)
     { // Bare data, accept it
       ReceivedData (packet, tcpHeader);
     }
