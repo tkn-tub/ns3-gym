900eec4066974d97154421d6f7d710d6f20dc3e0
Peter D. Barnes, Jr. <barnes26@llnl.gov>
2012-11-05 13:52:58 -0800

    [Bug 1521]  Have doxygen log more warnings; remove obsolete items from log report script.

--- doc/doxygen.warnings.report.sh
+++ doc/doxygen.warnings.report.sh
@@ -23,42 +23,12 @@ echo
 undocparam=` \
 grep -v "^$rootdir" $logfile | \
     grep -v "not generated, too many nodes" | \
-    grep "^  parameter '" | \
+    grep "^  parameter '" $logfile | \
     wc -l | \
     sed 's/^[ \t]*//;s/[ \t]*$//' `
 
-echo "+ $undocparam undocumented parameters."
+echo "+ $undocparam additional undocumented parameters."
 echo
 
-
-function sort_tags {
-    local conf=${1:-doxygen.conf}
-    local tags=${2:-$1.tags}
-    grep -v "^#" $conf | \
-	sed '/^$/d' | \
-	sed ':a;/\\$/N;s/\\\n/|n /;ta' | \
-	sort | \
-	sed 's/|n /\\\n/g' > $tags
-}
-
-orig=doxygen-r7657.conf
-conf=doxygen.conf
-tags=doxygen.conf.tags
-delt=$tags.changed
-
-sort_tags $orig
-sort_tags $conf $tags
-
-# Show only changed lines from second file
-diff --changed-group-format='%>' \
-     --unchanged-group-format='' \
-     $orig.tags $tags > $delt
-
-deltn=`cat $delt | \
-    wc -l | \
-    sed 's/^[ \t]*//;s/[ \t]*$//' `
-
-echo "R7657 sorted tags in:    $orig.tags"
-echo "Sorted doxygen tags in:  $tags"
-echo "Changed doxygen tags in: $delt, ($deltn lines)"
+echo "(All counts are lower bounds.)"
 echo
