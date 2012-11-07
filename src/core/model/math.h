e8333efaf50c73b8ead4cdf2041e9a65a05f4f90
Tom Henderson <tomh@tomh.org>
2012-11-06 21:35:13 -0800

    add license to file

--- src/core/model/math.h
+++ src/core/model/math.h
@@ -1,3 +1,23 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ * FreeBSD log2 patch from graphviz port to FreeBSD 9, courtesy of
+ * Christoph Moench-Tegeder <cmt@burggraben.net>
+ */
+
 #ifndef MATH_H
 #define MATH_H
 
