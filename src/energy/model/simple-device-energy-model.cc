3f41bbf326fac4e1abf2a9b00b4dabc4d85a4783
Vedran Miletić <rivanvx@gmail.com>
2013-04-04 21:35:09 +0200

    Add proper license header to simple-device-energy-model.{h,cc}

--- src/energy/model/simple-device-energy-model.cc
+++ src/energy/model/simple-device-energy-model.cc
@@ -1,3 +1,23 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2010 Andrea Sacco
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Andrea Sacco <andrea.sacco85@gmail.com>
+ */
+
 #include "ns3/simulator.h"
 #include "ns3/trace-source-accessor.h"
 #include "ns3/energy-source.h"
