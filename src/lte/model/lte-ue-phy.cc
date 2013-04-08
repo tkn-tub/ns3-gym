ea9118adf96d44ac590cbe5558f5d4f7fa5f6078
Marco Miozzo <marco.miozzo@cttc.es>
2013-04-08 16:25:35 +0200

    Bug-fix: initialization missing for evaluating SINR in PHY Traces

--- src/lte/model/lte-ue-phy.cc
+++ src/lte/model/lte-ue-phy.cc
@@ -457,6 +457,8 @@ LteUePhy::GenerateCtrlCqiReport (const SpectrumValue& sinr)
         }
       double rsrp = sum / (double)rbNum;
       // averaged SINR among RBs
+      sum = 0.0;
+      rbNum = 0;
       for (it = sinr.ConstValuesBegin (); it != sinr.ConstValuesEnd (); it++)
         {
           sum += (*it);
