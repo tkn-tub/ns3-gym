0e8b81cd0932cf21bc52ec6074e43c1226afeba0
Alina Quereilhac <alina.quereilhac@inria.fr>
2013-05-04 22:38:49 +0200

    fixed Bug 1649 - bindings scanning for fd-net-device

--- src/fd-net-device/bindings/modulegen_customizations.py
+++ src/fd-net-device/bindings/modulegen_customizations.py
@@ -4,16 +4,21 @@ def post_register_types(root_module):
     enabled_features = os.environ['NS3_ENABLED_FEATURES'].split(',')
 
     if 'EmuFdNetDevice' not in enabled_features:
-        root_module.classes.remove(root_module['ns3::EmuFdNetDeviceHelper'])
+        if 'ns3::EmuFdNetDeviceHelper'in root_module:
+	    root_module.classes.remove(root_module['ns3::EmuFdNetDeviceHelper'])
 
     if 'TapFdNetDevice' not in enabled_features:
-        root_module.classes.remove(root_module['ns3::TapFdNetDeviceHelper'])
+        if 'ns3::TapFdNetDeviceHelper'in root_module:
+            root_module.classes.remove(root_module['ns3::TapFdNetDeviceHelper'])
 
     if 'PlanetLabFdNetDevice' not in enabled_features:
-        root_module.classes.remove(root_module['ns3::PlanetLabFdNetDeviceHelper'])
+        if 'ns3::PlanetLabFdNetDeviceHelper'in root_module:
+            root_module.classes.remove(root_module['ns3::PlanetLabFdNetDeviceHelper'])
 
     if 'FdNetDevice' not in enabled_features:
         for clsname in ['FdNetDevice', 'FdNetDeviceHelper', 'FdNetDeviceFdReader']: 
-            root_module.classes.remove(root_module['ns3::%s' % clsname])
-        root_module.enums.remove(root_module['ns3::FdNetDeviceHelper::EncapsulationMode'])
+            if 'ns3::%s' % clsname in root_module:
+                root_module.classes.remove(root_module['ns3::%s' % clsname])
+        if 'ns3::FdNetDeviceHelper::EncapsulationMode' in root_module:
+            root_module.enums.remove(root_module['ns3::FdNetDeviceHelper::EncapsulationMode'])
 
