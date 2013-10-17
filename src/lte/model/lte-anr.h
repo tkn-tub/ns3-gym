efc6f7e6c8dae5b262144aa98ccddc079942e6b9
Budiarto Herman <budiarto.herman@magister.fi>
2013-10-17 17:21:14 +0300

    Correction to copyright notices in some GSoC LTE code

--- src/lte/model/lte-anr.h
+++ src/lte/model/lte-anr.h
@@ -1,5 +1,6 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
+ * Copyright (c) 2011, 2012 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
  * Copyright (c) 2013 Budiarto Herman
  *
  * This program is free software; you can redistribute it and/or modify
@@ -15,8 +16,13 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Author: Budiarto Herman <budiarto.herman@magister.fi>
+ * Original work authors (from lte-enb-rrc.cc):
+ * - Nicola Baldo <nbaldo@cttc.es>
+ * - Marco Miozzo <mmiozzo@cttc.es>
+ * - Manuel Requena <manuel.requena@cttc.es>
  *
+ * Converted to ANR interface by:
+ * - Budiarto Herman <budiarto.herman@magister.fi>
  */
 
 #ifndef LTE_ANR_H
--- src/lte/model/lte-enb-rrc.cc
+++ src/lte/model/lte-enb-rrc.cc
@@ -15,9 +15,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Author: Nicola Baldo <nbaldo@cttc.es>
- *         Marco Miozzo <mmiozzo@cttc.es>
- *         Manuel Requena <manuel.requena@cttc.es>
+ * Authors: Nicola Baldo <nbaldo@cttc.es>
+ *          Marco Miozzo <mmiozzo@cttc.es>
+ *          Manuel Requena <manuel.requena@cttc.es>
  */
 
 #include "lte-enb-rrc.h"
