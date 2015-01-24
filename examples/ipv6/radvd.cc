f651927bc894343b1f4ce446de3a85ed6cae6c89
Tommaso Pecorella <tommaso.pecorella@unifi.it>
2015-01-24 23:16:24 +0100

    Simplify Radvd examples - remove unnecessary calls

--- examples/ipv6/radvd.cc
+++ examples/ipv6/radvd.cc
@@ -97,7 +97,6 @@ int main (int argc, char** argv)
   tmp2.Add (d1.Get (1)); /* R */
   Ipv6InterfaceContainer iicr1 = ipv6.Assign (tmp2); /* R interface to the first subnet is just statically assigned */
   iicr1.SetForwarding (0, true);
-  iicr1.SetDefaultRouteInAllNodes (0);
   iic1.Add (iicr1);
 
   /* second subnet R - n1 */
@@ -106,7 +105,6 @@ int main (int argc, char** argv)
   tmp3.Add (d2.Get (0)); /* R */
   Ipv6InterfaceContainer iicr2 = ipv6.Assign (tmp3); /* R interface */
   iicr2.SetForwarding (0, true);
-  iicr2.SetDefaultRouteInAllNodes (0);
 
   NetDeviceContainer tmp4;
   tmp4.Add (d2.Get (1)); /* n1 */
