6e9673548726b8ee156c6fb209d30076817cb3b4
Nicola Baldo <nbaldo@cttc.es>
2015-01-29 11:36:56 +0100

    fixed Bug 2042 - src/lte/model/a3-rsrp-handover-algorithm.cc:137:79: error: overflow in implicit constant conversion

--- src/lte/model/a3-rsrp-handover-algorithm.cc
+++ src/lte/model/a3-rsrp-handover-algorithm.cc
@@ -134,7 +134,7 @@ A3RsrpHandoverAlgorithm::DoReportUeMeas (uint16_t rnti,
           && !measResults.measResultListEutra.empty ())
         {
           uint16_t bestNeighbourCellId = 0;
-          uint8_t bestNeighbourRsrp = -std::numeric_limits<double>::infinity ();
+          uint8_t bestNeighbourRsrp = 0;
 
           for (std::list <LteRrcSap::MeasResultEutra>::iterator it = measResults.measResultListEutra.begin ();
                it != measResults.measResultListEutra.end ();
