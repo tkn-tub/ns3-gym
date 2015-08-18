c4eb076d2a519265840e600a18982df4a2b555f1
Peter D. Barnes, Jr. <barnes26@llnl.gov>
2015-08-18 16:33:58 -0700

    Spell out organization name in copyright notice.

--- src/mesh/helper/mesh-stack-installer.cc
+++ src/mesh/helper/mesh-stack-installer.cc
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2015, LLNL
+ * Copyright (c) 2015, Lawrence Livermore National Laboratory
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
