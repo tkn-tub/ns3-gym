7907f2154e4cd1fbc7a6361f54e0ee84a9a6c234
Tommaso Pecorella <tommaso.pecorella@unifi.it>
2015-11-05 12:42:24 +0100

    Clarify PacketTagList::Add assert message

--- src/network/model/packet-tag-list.cc
+++ src/network/model/packet-tag-list.cc
@@ -247,7 +247,7 @@ PacketTagList::Add (const Tag &tag) const
   // ensure this id was not yet added
   for (struct TagData *cur = m_next; cur != 0; cur = cur->next) 
     {
-      NS_ASSERT (cur->tid != tag.GetInstanceTypeId ());
+      NS_ASSERT_MSG (cur->tid != tag.GetInstanceTypeId (), "Error: cannot add the same kind of tag twice.");
     }
   struct TagData * head = new struct TagData ();
   head->count = 1;
