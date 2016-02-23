e478244f376ab136bd8af56416db07f13d843697
Natale Patriciello <natale.patriciello@gmail.com>
2016-02-23 14:54:57 +0100

    internet: added __STDC_LIMIT_MACROS define

--- src/internet/test/tcp-general-test.cc
+++ src/internet/test/tcp-general-test.cc
@@ -16,6 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
+#define __STDC_LIMIT_MACROS
 #include "ns3/test.h"
 #include "ns3/node-container.h"
 #include "ns3/tcp-socket-base.h"
