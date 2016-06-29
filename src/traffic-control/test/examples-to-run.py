8300bf4e38c2d523de13511ecb1aeb1e3074c7ef
Mohit P. Tahiliani <tahiliani.nitk@gmail.com>
2016-06-29 18:28:02 +0200

    traffic-control: (fixes #2448) Run the examples in the test suite

---
+++ src/traffic-control/test/examples-to-run.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+## -*- Mode: python; py-indent-offset: 4; indent-tabs-mode: nil; coding: utf-8; -*-
+
+# A list of C++ examples to run in order to ensure that they remain
+# buildable and runnable over time.  Each tuple in the list contains
+#
+#     (example_name, do_run, do_valgrind_run).
+#
+# See test.py for more information.
+cpp_examples = [
+    ("adaptive-red-tests --testNumber=1", "True", "True"),
+    ("adaptive-red-tests --testNumber=2", "True", "True"),
+    ("adaptive-red-tests --testNumber=6", "True", "True"),
+    ("adaptive-red-tests --testNumber=7", "True", "True"),
+    ("adaptive-red-tests --testNumber=8", "True", "True"),
+    ("adaptive-red-tests --testNumber=9", "True", "True"),
+    ("adaptive-red-tests --testNumber=10", "True", "True"),
+    ("adaptive-red-tests --testNumber=12", "True", "True"),
+    ("adaptive-red-tests --testNumber=13", "True", "True"),
+    ("adaptive-red-tests --testNumber=14", "True", "True"),
+    ("adaptive-red-tests --testNumber=15", "True", "True"),
+    ("codel-vs-pfifo-asymmetric --routerWanQueueDiscType=PfifoFast --simDuration=10", "True", "True"),
+    ("codel-vs-pfifo-asymmetric --routerWanQueueDiscType=CoDel --simDuration=10", "True", "True"),
+    ("codel-vs-pfifo-basic-test --queueDiscType=PfifoFast --simDuration=10", "True", "True"),
+    ("codel-vs-pfifo-basic-test --queueDiscType=CoDel --simDuration=10", "True", "True"),
+    ("pfifo-vs-red --queueDiscType=PfifoFast", "True", "True"),
+    ("pfifo-vs-red --queueDiscType=PfifoFast --modeBytes=1", "True", "True"),
+    ("pfifo-vs-red --queueDiscType=RED", "True", "True"),
+    ("pfifo-vs-red --queueDiscType=RED --modeBytes=1", "True", "True"),
+    ("red-tests --testNumber=1", "True", "True"),
+    ("red-tests --testNumber=3", "True", "True"),
+    ("red-tests --testNumber=4", "True", "True"),
+    ("red-tests --testNumber=5", "True", "True"),
+    ("red-vs-ared --queueDiscType=RED", "True", "True"),
+    ("red-vs-ared --queueDiscType=RED --modeBytes=true", "True", "True"),
+    ("red-vs-ared --queueDiscType=ARED", "True", "True"),
+    ("red-vs-ared --queueDiscType=ARED --modeBytes=true", "True", "True"),
+]
+
+# A list of Python examples to run in order to ensure that they remain
+# runnable over time.  Each tuple in the list contains
+#
+#     (example_name, do_run).
+#
+# See test.py for more information.
+python_examples = []
