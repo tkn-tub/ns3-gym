ab4ddafe280a804c2f9d239095d7238a864a2736
Peter D. Barnes, Jr. <barnes26@llnl.gov>
2016-09-02 22:08:32 -0400

    [network] Clarify logging macros

--- src/network/test/sequence-number-test-suite.cc
+++ src/network/test/sequence-number-test-suite.cc
@@ -98,84 +98,84 @@ SequenceNumberTestCase::SequenceNumberTracer (SequenceNumber32 oldval, SequenceN
 
 void SequenceNumberTestCase::DoRun (void)
 {
-#define NS_TEST_ASSERT_EQUAL(a,b) NS_TEST_ASSERT_MSG_EQ (a,b, "foo")
-#define NS_TEST_ASSERT(a) NS_TEST_ASSERT_MSG_EQ (bool(a), true, "foo")
+#define SEQ_TEST_ASSERT_EQUAL(a,b) NS_TEST_ASSERT_MSG_EQ (a,b, "foo")
+#define SEQ_TEST_ASSERT(a) NS_TEST_ASSERT_MSG_EQ (bool(a), true, "foo")
 
   {
     SequenceNumber32 num1 (3), num2 (5);
     uint32_t value;
 
     value = (num1 + num2).GetValue ();
-    NS_TEST_ASSERT_EQUAL (value, 8);
+    SEQ_TEST_ASSERT_EQUAL (value, 8);
 
     num1 += num2.GetValue ();
-    NS_TEST_ASSERT_EQUAL (num1, SequenceNumber32 (8));
+    SEQ_TEST_ASSERT_EQUAL (num1, SequenceNumber32 (8));
 
     ++num1;
-    NS_TEST_ASSERT_EQUAL (num1, SequenceNumber32 (9));
+    SEQ_TEST_ASSERT_EQUAL (num1, SequenceNumber32 (9));
 
     --num1;
-    NS_TEST_ASSERT_EQUAL (num1, SequenceNumber32 (8));
+    SEQ_TEST_ASSERT_EQUAL (num1, SequenceNumber32 (8));
 
     num1++;
-    NS_TEST_ASSERT_EQUAL (num1, SequenceNumber32 (9));
+    SEQ_TEST_ASSERT_EQUAL (num1, SequenceNumber32 (9));
 
     num1--;
-    NS_TEST_ASSERT_EQUAL (num1, SequenceNumber32 (8));
+    SEQ_TEST_ASSERT_EQUAL (num1, SequenceNumber32 (8));
 
   }
 
   {
     SequenceNumber16 num1 (60900), num2 (5), num3 (10000);
 
-    NS_TEST_ASSERT (num1 == num1);
+    SEQ_TEST_ASSERT (num1 == num1);
 
-    NS_TEST_ASSERT (num2 != num1);
+    SEQ_TEST_ASSERT (num2 != num1);
 
-    NS_TEST_ASSERT (num3 > num2);
-    NS_TEST_ASSERT (num3 >= num2);
-    NS_TEST_ASSERT (num1 < num3);
-    NS_TEST_ASSERT (num1 <= num3);
+    SEQ_TEST_ASSERT (num3 > num2);
+    SEQ_TEST_ASSERT (num3 >= num2);
+    SEQ_TEST_ASSERT (num1 < num3);
+    SEQ_TEST_ASSERT (num1 <= num3);
 
-    NS_TEST_ASSERT (num1 < num2);
-    NS_TEST_ASSERT (num1 <= num2);
-    NS_TEST_ASSERT (num2 > num1);
-    NS_TEST_ASSERT (num2 >= num1);
+    SEQ_TEST_ASSERT (num1 < num2);
+    SEQ_TEST_ASSERT (num1 <= num2);
+    SEQ_TEST_ASSERT (num2 > num1);
+    SEQ_TEST_ASSERT (num2 >= num1);
 
-    NS_TEST_ASSERT (num1+num2 > num1);
-    NS_TEST_ASSERT (num1+num2 >= num1);
-    NS_TEST_ASSERT (num1 < num1+num2);
-    NS_TEST_ASSERT (num1 <= num1+num2);
+    SEQ_TEST_ASSERT (num1+num2 > num1);
+    SEQ_TEST_ASSERT (num1+num2 >= num1);
+    SEQ_TEST_ASSERT (num1 < num1+num2);
+    SEQ_TEST_ASSERT (num1 <= num1+num2);
 
-    NS_TEST_ASSERT (num1 < num1+num3);
-    NS_TEST_ASSERT (num1 <= num1+num3);
-    NS_TEST_ASSERT (num1+num3 > num1);
-    NS_TEST_ASSERT (num1+num3 >= num1);
+    SEQ_TEST_ASSERT (num1 < num1+num3);
+    SEQ_TEST_ASSERT (num1 <= num1+num3);
+    SEQ_TEST_ASSERT (num1+num3 > num1);
+    SEQ_TEST_ASSERT (num1+num3 >= num1);
   }
 
   {
-    NS_TEST_ASSERT_EQUAL ((SequenceNumber16 (1000) + SequenceNumber16 (6000)) - SequenceNumber16 (1000), 6000);
-    NS_TEST_ASSERT_EQUAL ((SequenceNumber16 (60000) + SequenceNumber16 (6000)) - SequenceNumber16 (60000), 6000);
-    NS_TEST_ASSERT_EQUAL (SequenceNumber16 (1000) - SequenceNumber16 (6000), -5000);
-    NS_TEST_ASSERT_EQUAL ((SequenceNumber16 (60000) + SequenceNumber16 (1000)) - SequenceNumber16 (65000), -4000);
+    SEQ_TEST_ASSERT_EQUAL ((SequenceNumber16 (1000) + SequenceNumber16 (6000)) - SequenceNumber16 (1000), 6000);
+    SEQ_TEST_ASSERT_EQUAL ((SequenceNumber16 (60000) + SequenceNumber16 (6000)) - SequenceNumber16 (60000), 6000);
+    SEQ_TEST_ASSERT_EQUAL (SequenceNumber16 (1000) - SequenceNumber16 (6000), -5000);
+    SEQ_TEST_ASSERT_EQUAL ((SequenceNumber16 (60000) + SequenceNumber16 (1000)) - SequenceNumber16 (65000), -4000);
   }
 
   {
     SequenceNumber32 num1 (3);
 
-    NS_TEST_ASSERT_EQUAL (num1 + 10, SequenceNumber32 (13));
+    SEQ_TEST_ASSERT_EQUAL (num1 + 10, SequenceNumber32 (13));
     num1 += -1;
-    NS_TEST_ASSERT_EQUAL (num1, SequenceNumber32 (2));
+    SEQ_TEST_ASSERT_EQUAL (num1, SequenceNumber32 (2));
 
-    NS_TEST_ASSERT_EQUAL (num1 - (num1 - 100), 100);
+    SEQ_TEST_ASSERT_EQUAL (num1 - (num1 - 100), 100);
   }
 
   {
     Ptr<SequenceNumberTestObj> obj = CreateObject<SequenceNumberTestObj> ();
     obj->TraceConnectWithoutContext ("TestTracedSequenceNumber", MakeCallback (&SequenceNumberTestCase::SequenceNumberTracer, this));
     obj->IncSequenceNumber ();
-    NS_TEST_ASSERT_EQUAL (m_oldval, SequenceNumber32 (0));
-    NS_TEST_ASSERT_EQUAL (m_newval, SequenceNumber32 (1));
+    SEQ_TEST_ASSERT_EQUAL (m_oldval, SequenceNumber32 (0));
+    SEQ_TEST_ASSERT_EQUAL (m_newval, SequenceNumber32 (1));
     obj->Dispose ();
   }
 
