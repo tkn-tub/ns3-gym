2fa3c9bbc6c5573a0b0b9ccb9bd0166783042d6e
Peter D. Barnes, Jr. <barnes26@llnl.gov>
2016-09-03 01:06:28 -0400

    [des-metrics] Missing include.

--- src/core/examples/main-test-sync.cc
+++ src/core/examples/main-test-sync.cc
@@ -16,6 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include "ns3/command-line.h"
 #include "ns3/simulator.h"
 #include "ns3/realtime-simulator-impl.h"
 #include "ns3/nstime.h"
