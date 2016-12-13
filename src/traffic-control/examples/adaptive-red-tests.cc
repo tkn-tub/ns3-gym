3fa947daf68a8718cf1d9661f17b868924b9e902
Mohit P. Tahiliani <tahiliani.nitk@gmail.com>
2016-12-13 19:57:00 +0100

    traffic-control: Fix ARED tests after introduction of ECN support

--- src/traffic-control/examples/adaptive-red-tests.cc
+++ src/traffic-control/examples/adaptive-red-tests.cc
@@ -481,42 +481,22 @@ main (int argc, char *argv[])
 
   RedQueueDisc::Stats st = StaticCast<RedQueueDisc> (queueDiscs.Get (0))->GetStats ();
 
+  if (st.unforcedDrop == 0)
+    {
+      std::cout << "There should be some unforced drops" << std::endl;
+      exit (1);
+    }
+
   if (aredTest == 1 || aredTest == 2 || aredTest == 13)
     {
-      if (st.unforcedDrop > st.forcedDrop)
-        {
-          std::cout << "Drops due to prob mark should be less than the drops due to hard mark" << std::endl;
-          exit (1);
-        }
-
       if (st.qLimDrop == 0)
         {
           std::cout << "There should be some drops due to queue full" << std::endl;
           exit (1);
         }
     }
-  else if (aredTest == 6 || aredTest == 7 || aredTest == 8 || aredTest == 9 || aredTest == 10 || aredTest == 14 || aredTest ==15)
+  else
     {
-      if (st.unforcedDrop > st.forcedDrop)
-        {
-          std::cout << "Drops due to prob mark should be less than the drops due to hard mark" << std::endl;
-          exit (1);
-        }
-
-      if (st.qLimDrop != 0)
-        {
-          std::cout << "There should be zero drops due to queue full" << std::endl;
-          exit (1);
-        }
-    }
-  else if (aredTest == 12)
-    {
-      if (st.unforcedDrop < st.forcedDrop)
-        {
-          std::cout << "Drops due to prob mark should be more than the drops due to hard mark" << std::endl;
-          exit (1);
-        }
-
       if (st.qLimDrop != 0)
         {
           std::cout << "There should be zero drops due to queue full" << std::endl;
--- src/traffic-control/examples/red-vs-ared.cc
+++ src/traffic-control/examples/red-vs-ared.cc
@@ -168,33 +168,16 @@ int main (int argc, char *argv[])
 
   RedQueueDisc::Stats st = StaticCast<RedQueueDisc> (queueDiscs.Get (0))->GetStats ();
 
-  if (queueDiscType == "RED")
+  if (st.unforcedDrop == 0)
     {
-      if (st.unforcedDrop > st.forcedDrop)
-        {
-          std::cout << "Drops due to prob mark should be less than the drops due to hard mark" << std::endl;
-          exit (1);
-        }
-
-      if (st.qLimDrop != 0)
-        {
-          std::cout << "There should be zero drops due to queue full" << std::endl;
-          exit (1);
-        }
+      std::cout << "There should be some unforced drops" << std::endl;
+      exit (1);
     }
-  else if (queueDiscType == "ARED")
-    {
-      if (st.unforcedDrop < st.forcedDrop)
-        {
-          std::cout << "Drops due to prob mark should be more than the drops due to hard mark" << std::endl;
-          exit (1);
-        }
 
-      if (st.qLimDrop != 0)
-        {
-          std::cout << "There should be zero drops due to queue full" << std::endl;
-          exit (1);
-        }
+  if (st.qLimDrop != 0)
+    {
+      std::cout << "There should be zero drops due to queue full" << std::endl;
+      exit (1);
     }
 
   std::cout << "*** Stats from the bottleneck queue disc ***" << std::endl;
