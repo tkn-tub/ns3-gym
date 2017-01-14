2de69f5590874fcc9efea8cfcaf064f6465bdad0
Tom Henderson <tomh@tomh.org>
2017-01-14 08:22:50 -0800

    traffic-control: (fixes #2617) PfifoFastQueueDisc::Peek all bands

--- src/traffic-control/model/pfifo-fast-queue-disc.cc
+++ src/traffic-control/model/pfifo-fast-queue-disc.cc
@@ -121,10 +121,12 @@ PfifoFastQueueDisc::DoPeek (void) const
 
   for (uint32_t i = 0; i < GetNInternalQueues (); i++)
     {
-      item = StaticCast<const QueueDiscItem> (GetInternalQueue (i)->Peek ());
-      NS_LOG_LOGIC ("Peeked from band " << i << ": " << item);
-      NS_LOG_LOGIC ("Number packets band " << i << ": " << GetInternalQueue (i)->GetNPackets ());
-      return item;
+      if ((item = StaticCast<const QueueDiscItem> (GetInternalQueue (i)->Peek ())) != 0)
+        {
+          NS_LOG_LOGIC ("Peeked from band " << i << ": " << item);
+          NS_LOG_LOGIC ("Number packets band " << i << ": " << GetInternalQueue (i)->GetNPackets ());
+          return item;
+        }
     }
 
   NS_LOG_LOGIC ("Queue empty");
