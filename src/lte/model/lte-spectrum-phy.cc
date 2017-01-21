d9256aaf3f83c6b4a62eeb30fcd94c1c14d80ba9
Biljana Bojovic <biljana.bojovic@gmail.com>
2017-01-21 12:26:42 +0100

    lte: initialize parameter member in lte-spectum-phy before use

--- src/lte/model/lte-spectrum-phy.cc
+++ src/lte/model/lte-spectrum-phy.cc
@@ -114,8 +114,9 @@ NS_OBJECT_ENSURE_REGISTERED (LteSpectrumPhy);
 LteSpectrumPhy::LteSpectrumPhy ()
   : m_state (IDLE),
     m_cellId (0),
-  m_transmissionMode (0),
-  m_layersNum (1)
+	m_componentCarrierId (0),
+    m_transmissionMode (0),
+    m_layersNum (1)
 {
   NS_LOG_FUNCTION (this);
   m_random = CreateObject<UniformRandomVariable> ();
