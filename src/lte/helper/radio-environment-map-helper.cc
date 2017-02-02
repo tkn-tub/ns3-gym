af1dc022b1bf3774d977965463ca54db2c5ed6f7
Luciano Jerez Chaves <ljerezchaves@gmail.com>
2017-02-02 16:49:55 +0100

    lte: (fixes #2632) build error due to uninitialized variable in rem helper

--- src/lte/helper/radio-environment-map-helper.cc
+++ src/lte/helper/radio-environment-map-helper.cc
@@ -282,8 +282,8 @@ RadioEnvironmentMapHelper::RunOneIteration (double xMin, double xMax, double yMi
 {
   NS_LOG_FUNCTION (this << xMin << xMax << yMin << yMax);
   std::list<RemPoint>::iterator remIt = m_rem.begin ();
-  double x;
-  double y;
+  double x = 0.0;
+  double y = 0.0;
   for (x = xMin; x < xMax + 0.5*m_xStep; x += m_xStep)
     {
       for (y = (x == xMin) ? yMin : m_yMin;
