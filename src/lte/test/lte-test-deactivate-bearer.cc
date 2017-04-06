411e998601359042878781940ea0ce6756f58df0
Alexander Krotov <ilabdsf@gmail.com>
2017-04-07 00:27:47 +0300

    lte: Install only one sink on remoteHost in lte-test-deactivate-bearer

--- src/lte/test/lte-test-deactivate-bearer.cc
+++ src/lte/test/lte-test-deactivate-bearer.cc
@@ -258,11 +258,12 @@ LenaDeactivateBearerTestCase::DoRun (void)
   PacketSinkHelper ulPacketSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), ulPort));
   ApplicationContainer clientApps;
   ApplicationContainer serverApps;
+
+  serverApps.Add (ulPacketSinkHelper.Install (remoteHost));  // receive packets from UEs
   for (uint32_t u = 0; u < ueNodes.GetN (); ++u)
     {
       ++ulPort;
       serverApps.Add (dlPacketSinkHelper.Install (ueNodes.Get (u))); // receive packets from remotehost
-      serverApps.Add (ulPacketSinkHelper.Install (remoteHost));  // receive packets from UEs
 
       UdpClientHelper dlClient (ueIpIface.GetAddress (u), dlPort); // uplink packets generator
       dlClient.SetAttribute ("Interval", TimeValue (MilliSeconds (m_interval)));
