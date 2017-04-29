bd2a27113a72193fba48667145bb4168ab93da4f
Tommaso Pecorella <tommaso.pecorella@unifi.it>
2017-04-30 00:05:35 +0200

    applications: (fixes #2732) BulkSend Tx trace is fired too much

--- src/applications/model/bulk-send-application.cc
+++ src/applications/model/bulk-send-application.cc
@@ -194,11 +194,11 @@ void BulkSendApplication::SendData (void)
 
       NS_LOG_LOGIC ("sending packet at " << Simulator::Now ());
       Ptr<Packet> packet = Create<Packet> (toSend);
-      m_txTrace (packet);
       int actual = m_socket->Send (packet);
       if (actual > 0)
         {
           m_totBytes += actual;
+          m_txTrace (packet);
         }
       // We exit this loop when actual < toSend as the send side
       // buffer is full. The "DataSent" callback will pop when
