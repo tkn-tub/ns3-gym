d6ea6b989ff3879e0e48d19f4bae03ccb06861ef
Sébastien Deronne <sebastien.deronne@gmail.com>
2017-11-04 10:01:02 +0100

    wifi: cleanup QosUtilsMapTidToAc

--- src/wifi/model/qos-utils.cc
+++ src/wifi/model/qos-utils.cc
@@ -31,26 +31,18 @@ QosUtilsMapTidToAc (uint8_t tid)
   switch (tid)
     {
     case 0:
-      return AC_BE;
-      break;
-    case 1:
-      return AC_BK;
-      break;
-    case 2:
-      return AC_BK;
-      break;
     case 3:
       return AC_BE;
       break;
-    case 4:
-      return AC_VI;
+    case 1:
+    case 2:
+      return AC_BK;
       break;
+    case 4:
     case 5:
       return AC_VI;
       break;
     case 6:
-      return AC_VO;
-      break;
     case 7:
       return AC_VO;
       break;
