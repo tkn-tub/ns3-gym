38343165f88d96cef0dacf968c70669f5bcbfc6c
Biljana Bojovic <biljana.bojovic@gmail.com>
2017-11-16 17:27:56 +0100

    lte:(fixes #2151) Generate properly redundancy version in uplink HARQ (thanks to Richard A. Rouil)

--- src/lte/model/lte-harq-phy.cc
+++ src/lte/model/lte-harq-phy.cc
@@ -176,11 +176,19 @@ LteHarqPhy::UpdateUlHarqProcessStatus (uint16_t rnti, double mi, uint16_t infoBy
     }
   else
     {
-      if ((*it).second.at (7).size () == 3) // MAX HARQ RETX
+      if ((*it).second.at (0).size () == 3) // MAX HARQ RETX
         {
           // HARQ should be disabled -> discard info
           return;
         }
+      
+//       move current status back at the end to maintain full history
+      HarqProcessInfoList_t list = (*it).second.at (0);
+      for (uint8_t i = 0; i < list.size (); i++)
+        {
+          (*it).second.at (7).push_back (list.at (i));
+        }
+      
       HarqProcessInfoElement_t el;
       el.m_mi = mi;
       el.m_infoBits = infoBytes * 8;
