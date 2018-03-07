08e8241a0ee247344c83c24456612459777ad06a
Getachew Redieteab <getachew.redieteab@orange.com>
2018-03-07 22:19:29 +0100

    wifi: Fix incorrect bash formatting in wifi-trans-example.sh

--- src/wifi/examples/wifi-trans-example.sh
+++ src/wifi/examples/wifi-trans-example.sh
@@ -121,7 +121,7 @@ do
   for bw in "${bw_ac_ax[@]}"
   do
     #160 MHz not available in 2.4 GHz
-    [ ${std} = "11ax_2_4GHZ" ] && ([ ${bw} = "80" ] || [${bw} = "160" ]) && continue
+    [ ${std} = "11ax_2_4GHZ" ] && ([ ${bw} = "80" ] || [ ${bw} = "160" ]) && continue
     #for all other combinations continue
     echo "==============================================="
     echo "Run for wifi-trans-example for ${std} and ${bw} MHz"
