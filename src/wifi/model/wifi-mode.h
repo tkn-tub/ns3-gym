e0da658b7ad4f06ff20bb9583f4c1e5d9efef64c
Alexander Krotov <krotov@iitp.ru>
2018-05-03 12:07:54 +0300

    s/accross/across/

--- src/wifi/model/wifi-mode.h
+++ src/wifi/model/wifi-mode.h
@@ -266,7 +266,7 @@ class WifiModeFactory
 public:
   /**
    * \param uniqueName the name of the associated WifiMode. This name
-   *        must be unique accross _all_ instances.
+   *        must be unique across _all_ instances.
    * \param modClass the class of modulation
    * \param isMandatory true if this WifiMode is mandatory, false otherwise.
    * \param codingRate if convolutional coding is used for this rate
@@ -288,7 +288,7 @@ public:
 
   /**
    * \param uniqueName the name of the associated WifiMode. This name
-   *        must be unique accross _all_ instances.
+   *        must be unique across _all_ instances.
    * \param mcsValue the mcs value
    * \param modClass the class of modulation
    *
