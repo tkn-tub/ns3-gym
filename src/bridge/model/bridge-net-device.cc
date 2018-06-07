d9f77b2b9f4dff38dd5a14408dc46ee272c91fd9
Akin Soysal <akinsoysal@gmail.com>
2018-06-07 12:41:52 -0700

    bridge: (fixes #2772) Bridge doesn't learn from packets sent to it

--- src/bridge/model/bridge-net-device.cc
+++ src/bridge/model/bridge-net-device.cc
@@ -105,6 +105,7 @@ BridgeNetDevice::ReceiveFromDevice (Ptr<NetDevice> incomingPort, Ptr<const Packe
     case PACKET_HOST:
       if (dst48 == m_address)
         {
+          Learn (src48, incomingPort);
           m_rxCallback (this, packet, protocol, src);
         }
       break;
@@ -118,6 +119,7 @@ BridgeNetDevice::ReceiveFromDevice (Ptr<NetDevice> incomingPort, Ptr<const Packe
     case PACKET_OTHERHOST:
       if (dst48 == m_address)
         {
+          Learn (src48, incomingPort);
           m_rxCallback (this, packet, protocol, src);
         }
       else
